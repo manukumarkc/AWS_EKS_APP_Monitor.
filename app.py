beca728b18512c261c6e2bcb7197795fc0e9c3f5
Manu kumar K C <manukumarkc@MacBookAir.station>
2025-05-20 00:04:46 +0100

    new build

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template_string
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template_string(open("index.html").read())
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
